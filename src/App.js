990bdcf7a48cebe039acce4d649099302d155f7b
dwaynediddy <burrows.dwayne@icloud.com>
2021-03-08 14:34:12 +0100

    increment increasing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment } from './actions'
 
 function App() {
   const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch()
+  
   return (
     <div className="App">
       <h2>Coutner {counter}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button>-</button>
+      {isLogged ? 
+      <h3>Valuable info Shouldnt see if not logged in</h3> : ''}
     </div>
   );
 }
