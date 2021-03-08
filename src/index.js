af749abb74c3ce66587d3e26185901d913b043ab
dwaynediddy <burrows.dwayne@icloud.com>
2021-03-08 13:35:40 +0100

    adding reducers

--- src/index.js
+++ src/index.js
@@ -2,42 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
+import {createStore} from 'redux'
+import allReducers from './reducers/index'
 
-//STORE -> GLOBALIZED STATE
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
-//ACTION INCREMENT
-const increment = () => {
-  return {
-    type: 'INCREMENT',
-  }
-}
-
-const decrement = () => {
-  return {
-    type: 'DECREMENT',
-  }
-}
-
-//REDUCER
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-  }
-}
-
-let store = createStore(counter)
-
-//display 
-store.subscribe(() => {
-  console.log(store.getState())
-})
-
-// DISPATCH
-store.dispatch(increment())
 
 
 
